188b4202542f1aaacb717645569ab1d0cebfb3a3
Afonso Lopes <afonsolopes031@gmail.com>
2026-02-11 23:05:36 +0000

    removed local path from the skewed computation

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ import data
 # This is the main function 
 def main():
     # Load the data, transform into a pandas df and remove rows with empty values 
-    Hitters = data.csv_to_df('/Users/afonsolopes/Salary-Prediction-Boost/Hitters.csv')
+    Hitters = data.csv_to_df('path')
 
     # log-transform the Salary column 
     Hitters_log_transformed = data.log_transform_salaries(Hitters)
@@ -17,7 +17,7 @@ def main():
     "PutOuts", "Assists", "Errors"
     ]   
     response = Hitters_log_transformed['Salary']
-    
+
 
 
 if __name__ == '__main__':
